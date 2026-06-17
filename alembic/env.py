312b969bfecfd45ef88f3705bbf5bbe5587c77b1
Charles7458 <c.charles8547@gmail.com>
2026-06-17 23:18:12 +0530

    added render.yaml

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,10 @@ from sqlalchemy import pool
 from sqlmodel import SQLModel
 from models import Faqs, Users, Tickets, Conversations, Messages, Orders, Tracking
 from alembic import context
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -14,6 +17,13 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+config = context.config
+db_url = os.getenv("DB_URL")
+
+# 2. If it exists, inject it into the configuration
+if db_url:
+    config.set_main_option("sqlalchemy.url", db_url)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
